bcaa21dc2466658425baf9c3ab418c0b92cbe664
João Pereira <joao.pereira@comon.pt>
2020-05-12 21:48:19 +0100

    use componentDidMount to check window

--- src/pages/share.js
+++ src/pages/share.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import moment from 'moment';
 import 'moment/locale/pt';
 import 'animate.css';
@@ -7,31 +7,45 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Countdown from '../containers/countdown';
 
-function Share() {
-  moment.locale('pt');
-  let urlParams = null;
-  let color = null;
-  let background = null;
-  let urlTitle = null;
-  let title = 'Covid Countdown';
-  if (typeof window !== 'undefined') {
-    urlParams = new URLSearchParams(window.location.search);
-    color = urlParams.get('color') || 'gray';
-    background = urlParams.get('bg') || 'white';
-    urlTitle = urlParams.get('title') || null;
-    title = urlTitle ? `Covid Countdown | ${urlTitle}` : 'Covid Countdown';
+class Share extends Component {
+  state = {
+    color: 'gray',
+    background: 'white',
+    title: '',
+    urlTitle: 'Covid Countdown',
+  };
+
+  componentDidMount() {
+    moment.locale('pt');
+
+    if (typeof window !== 'undefined') {
+      const urlParams = new URLSearchParams(window.location.search);
+      const color = urlParams.get('color') || 'gray';
+      const background = urlParams.get('bg') || 'white';
+      const urlTitle = urlParams.get('title') || null;
+      const title = urlTitle
+        ? `Covid Countdown | ${urlTitle}`
+        : 'Covid Countdown';
+
+      this.setState({ color, background, title, urlTitle });
+    }
   }
-  return (
-    <Layout share color={color} background={background}>
-      <SEO
-        title={title}
-        keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
-      />
-      <div className="h-screen w-full flex flex-col justify-center">
-        <Countdown title={urlTitle} color={color} />
-      </div>
-    </Layout>
-  );
+
+  render() {
+    const { color, background, title, urlTitle } = this.state;
+
+    return (
+      <Layout share color={color} background={background}>
+        <SEO
+          title={title}
+          keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
+        />
+        <div className="h-screen w-full flex flex-col justify-center">
+          <Countdown title={urlTitle} color={color} />
+        </div>
+      </Layout>
+    );
+  }
 }
 
 export default Share;
